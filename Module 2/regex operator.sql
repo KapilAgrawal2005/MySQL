71f5bd6a5006744196291ccfc1a3c8f1bd2a831a
Kapil Agrawal <kapilagrawal448@gmail.com>
2025-07-24 20:53:57 +0530

    Update regex operator.sql

--- Module 2/regex operator.sql
+++ Module 2/regex operator.sql
@@ -28,3 +28,6 @@ SELECT * FROM employees WHERE firstName REGEXP "^[a-h]|lie$";
 
 -- customers whose phone numbers ends with 55
 SELECT * FROM customers WHERE phone REGEXP "55$";
+
+-- Query the list of CITY names from STATION which have vowels (i.e., a, e, i, o, and u) as both their first and last characters. Your result cannot contain duplicates.
+select distinct city from station where city regexp "^[aeiou].*[aeiou]$";
